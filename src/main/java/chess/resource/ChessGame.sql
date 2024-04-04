8acb477b65579d6db224316e4d221eb23106d89a
masonkimseoul <masonkimseoul@gmail.com>
2024-04-04 13:20:47 +0900

    refactor: DB 스키마 수정

--- src/main/java/chess/resource/ChessGame.sql
+++ src/main/java/chess/resource/ChessGame.sql
@@ -12,11 +12,12 @@ CREATE TABLE chessgame
 
 CREATE TABLE piece
 (
-    id               BIGINT NOT NULL AUTO_INCREMENT,
-    game_id          BIGINT NOT NULL,
-    piece_appearance VARCHAR(1),
-    position_column  VARCHAR(1),
-    position_row     VARCHAR(1),
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    game_id         BIGINT NOT NULL,
+    piece_type      VARCHAR(6),
+    piece_color     VARCHAR(5),
+    position_column VARCHAR(1),
+    position_row    VARCHAR(1),
     PRIMARY KEY (id),
     FOREIGN KEY (game_id) REFERENCES chessgame (id) ON DELETE CASCADE
 );
